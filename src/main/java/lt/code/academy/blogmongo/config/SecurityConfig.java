9a4dbaf8afafea38316d6d026200987dae40eccb
TomasBaniulis <119011344+TomasBaniulis@users.noreply.github.com>
2023-05-15 20:06:32 +0300

    last minute update:)

--- src/main/java/lt/code/academy/blogmongo/config/SecurityConfig.java
+++ src/main/java/lt/code/academy/blogmongo/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package lt.code.academy.blogmongo.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     @Bean
